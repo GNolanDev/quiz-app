d80ca2623524741608fccaf3d215aa47efb0db51
gingergaz <gaz@cliffhanger.com>
2021-08-24 17:17:08 +0100

    Add Topics slice & export selector, action and reducer

---
+++ src/features/topics/TopicsSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// set up topics slice with action for adding a new topic
+const options = {
+  name: topics,
+  initialState: { topics: {} },
+  reducers: {
+    addTopic: (state, action) => {
+      state.topics[action.payload.id] = { ...action.payload, quizIds: [] };
+    }
+  }
+}
+
+const topicsSlice = createSlice(options);
+
+export const selectTopics = state => state.topics;
+export const { addTopic } = topicsSlice.actions;
+export default topicsSlice.reducer;
