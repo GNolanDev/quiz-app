bf8fba8ce0c92cc3fc29436ac2acd06a104d201a
gingergaz <gaz@cliffhanger.com>
2021-08-25 12:38:03 +0100

    Add thunk action creator 'quizCreated' to QuizzesSlice to combine creating new quiz and adding to a topic

--- src/features/quizzes/QuizzesSlice.js
+++ src/features/quizzes/QuizzesSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addQuizToTopic } from "../topics/TopicsSlice";
 
 // set up quizzes slice
 const options = {
@@ -12,6 +13,14 @@ const options = {
 };
 
 const quizzesSlice = createSlice(options);
+
+// action creator for creating new quiz and adding to topic
+export const quizCreated = (quiz) => {
+  return (dispatch, getState) => {
+    dispatch(quizzesSlice.actions.addQuiz(quiz)); // adds full quiz detail
+    dispatch(addQuizToTopic({ topicId: quiz.topicId, quizId: quiz.id })); // adds only required data from quiz
+  };
+};
 
 // export selector
 export const selectQuizzes = (state) => state.quizzes;
