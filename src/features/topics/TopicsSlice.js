ab529f184e649e0b60089f8dd3869921ddc7ff4a
gingergaz <gaz@cliffhanger.com>
2021-08-25 11:01:04 +0100

    Reinstall with npm to get latest jest/babel etc. Fix typo in TopicsSlice

--- src/features/topics/TopicsSlice.js
+++ src/features/topics/TopicsSlice.js
@@ -1,18 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 // set up topics slice with action for adding a new topic
 const options = {
-  name: topics,
+  name: "topics",
   initialState: { topics: {} },
   reducers: {
     addTopic: (state, action) => {
       state.topics[action.payload.id] = { ...action.payload, quizIds: [] };
     }
   }
-}
+};
 
 const topicsSlice = createSlice(options);
 
-export const selectTopics = state => state.topics;
+export const selectTopics = (state) => state.topics;
 export const { addTopic } = topicsSlice.actions;
 export default topicsSlice.reducer;
